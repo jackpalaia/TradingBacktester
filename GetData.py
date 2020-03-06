464c49c465ee3b2d2a89b931ae39cff0751a0a17
jackpalaia <jack@jackpalaia.com>
2020-03-06 14:07:24 -0500

    added comments

--- GetData.py
+++ GetData.py
@@ -3,7 +3,9 @@ import pandas_datareader as web
 import datetime as dt
 import json
 
+
 def generatePriceDF():
+    """ Pulls stock data from start date to end date from yahoo finance """
     # getting start and end dates as well as stock tickers from settings.json
     with open('settings.json', 'r') as settings:
         data = json.load(settings);
@@ -13,19 +15,21 @@ def generatePriceDF():
     startDate = dt.datetime(start[0], start[1], start[2])
     endDate = dt.datetime(end[0], end[1], end[2])
     
+    # pulling data from yahoo finance, putting closing prices of each stock for each day in dataframe
     priceDF = pd.DataFrame()
     dfList = []
     for ticker in tickers:
         tempdf = pd.DataFrame()
         df = web.DataReader(ticker, 'yahoo', startDate, endDate)
-        tempdf[ticker] = round(df['Close'], 2)
+        tempdf[ticker] = round(df['Close'], 2) # rounding prices to 2 decimal points
         dfList.append(tempdf)
-    priceDF = pd.concat(dfList, axis=1, sort=False)
+    priceDF = pd.concat(dfList, axis=1, sort=False) # turning dataframe list into actual dataframe
 
     print(priceDF)
     return priceDF
 
 def generateNDayForwardReturn():
-    
+    """ Uses generatePriceDF() to generate N day forward return for each stock on each day """
+
 
 generatePriceDF()
