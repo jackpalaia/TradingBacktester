207c6e51e351bfad7eb94bbd91e27aef08998d04
jackpalaia <jack@jackpalaia.com>
2020-03-09 22:05:36 -0400

    started process for getting data from DIX.csv

--- GetIndicator.py
+++ GetIndicator.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import csv
+import json
+
+def getDixData():
+    """ Gets DIX index data from DIX.csv, dates are start and end date in settings.json """
+    with open('settings.json', 'r') as settings:
+        data = json.load(settings)
